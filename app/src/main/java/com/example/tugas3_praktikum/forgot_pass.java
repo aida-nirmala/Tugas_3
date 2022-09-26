c1e0eb6cf3fec27f60c1aed1188ad43c9709a30d
HAPIIIS <83536014+HAPIIIS@users.noreply.github.com>
2022-09-26 15:39:53 +0700

    Aida Nirmalasari_H1D020018_Shift H

---
+++ app/src/main/java/com/example/tugas3_praktikum/forgot_pass.java
@@ -0,0 +1,14 @@
+package com.example.tugas3_praktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgot_pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
---
+++ app/src/main/java/com/example/tugas3_praktikum/reset_pass.java
@@ -0,0 +1,14 @@
+package com.example.tugas3_praktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reset_pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass);
+    }
+}
